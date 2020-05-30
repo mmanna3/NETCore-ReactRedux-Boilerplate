be40a72a9a2bd6c421e92a98ef98efd1f68be408
Mmanna3 <matiasmannarino@gmail.com>
2020-05-30 15:06:57 -0300

    En producción, levanta frontend de carpeta dist

--- Hostelapp/Program.cs
+++ Hostelapp/Program.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Hostelapp.Persistence.Contexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Hostelapp
 {
@@ -16,6 +9,8 @@ namespace Hostelapp
     {
         public static void Main(string[] args)
         {
+            //Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Production");
+
             var host = BuildWebHost(args);
 
             using (var scope = host.Services.CreateScope())
--- Hostelapp/Startup.cs
+++ Hostelapp/Startup.cs
@@ -6,7 +6,6 @@ using Hostelapp.Persistence.Repositories;
 using Hostelapp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,7 +26,13 @@ namespace Hostelapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "dist";
+            });
 
             services.AddDbContext<AppDbContext>(options => {
                 options.UseSqlServer(Configuration["ConnectionStrings:Default"]);
@@ -58,6 +63,8 @@ namespace Hostelapp
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
@@ -65,7 +72,14 @@ namespace Hostelapp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "dist";
             });
         }
     }
