c6f30182a589f1cb6abd94a1367eb19a4806b7f5
Mmanna3 <matiasmannarino@gmail.com>
2020-06-06 22:17:00 -0300

    Agrega module.css de prueba

--- Frontend/src/components/Comment.js
+++ Frontend/src/components/Comment.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './Comment.module.css'
 
 export const Comment = ({ comment }) => (
-  <aside className="comment">
+  <aside className={styles.comment}>
     <h2>{comment.title}</h2>
     <h3>{comment.email}</h3>
     <p>{comment.body}</p>
