3cf148823478f43ec4b662cfc0bd91e67bbdade8
Mmanna3 <matiasmannarino@gmail.com>
2020-06-16 22:19:28 -0300

    Agrega entidad habitación y su migración

---
+++ Backend/Api/Core/Models/Habitacion.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Core.Models
+{
+    public class Habitacion
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(30)]
+        public string Nombre { get; set; }
+
+        public byte CamasIndividuales { get; set; }
+
+        public byte CamasMarineras { get; set; }
+
+        public byte CamasMatrimoniales { get; set; }
+    }
+}
--- Backend/Api/Persistence/Config/AppDbContext.cs
+++ Backend/Api/Persistence/Config/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace Api.Persistence.Config
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Habitacion> Habitaciones { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
