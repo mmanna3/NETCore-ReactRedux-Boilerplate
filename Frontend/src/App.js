ea052095c63d775c620c21a672559bc5a74d29be
Mmanna3 <matiasmannarino@gmail.com>
2020-06-24 02:36:20 -0300

    Comenta navbar para mostrarle a Lu

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,7 +6,7 @@ import {
   Redirect,
 } from 'react-router-dom'
 
-import { Navbar } from './components/Navbar'
+// import { Navbar } from './components/Navbar'
 import DashboardPage from './pages/DashboardPage'
 import PostsPage from './pages/PostsPage'
 import SinglePostPage from './pages/SinglePostPage'
@@ -15,10 +15,11 @@ import HabitacionesPage from 'features/habitaciones/Page'
 const App = () => {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Switch>
         <Route exact path="/habitaciones" component={HabitacionesPage} />
-        <Route exact path="/" component={DashboardPage} />        
+        <Route exact path="/" component={HabitacionesPage} />
+        <Route exact path="/dashboard" component={DashboardPage} />
         <Route exact path="/posts" component={PostsPage} />
         <Route exact path="/posts/:id" component={SinglePostPage} />
         <Redirect to="/" />
