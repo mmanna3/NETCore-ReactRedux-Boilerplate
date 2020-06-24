e075400b18fc1e1d74d59b2b409a00fcbb63dd32
Mmanna3 <matiasmannarino@gmail.com>
2020-06-24 02:24:31 -0300

    Agrega estado interno postFinished para resetear estado

--- Frontend/src/features/habitaciones/crear/slice.js
+++ Frontend/src/features/habitaciones/crear/slice.js
@@ -5,6 +5,7 @@ export const initialState = {
   loading: false,
   hasSuccess: false,
   hasErrors: false,
+  hasFinished: false,
   requestData: '',
   respuesta: '',
 }
@@ -15,21 +16,26 @@ const crearHabitacionSlice = createSlice({
   reducers: {
     post: (state, { requestData }) => {
       state.loading = true
+      state.hasSuccess = false
+      state.hasErrors = false
       state.requestData = requestData    
     },
     postSuccess: (state, { respuesta }) => {
       state.respuesta = respuesta
-      state.loading = false
       state.hasSuccess = true
     },
     postFailure: state => {
-      state.loading = false
       state.hasErrors = true
+    },
+    postFinished: state => {
+      state.loading = false
+      state.hasErrors = false
+      state.hasSuccess = false
     },
   },
 })
 
-export const { post, postSuccess, postFailure } = crearHabitacionSlice.actions
+export const { post, postSuccess, postFailure, postFinished } = crearHabitacionSlice.actions
 export const crearHabitacionSelector = state => state.crearHabitacion
 export default crearHabitacionSlice.reducer
 
@@ -44,6 +50,8 @@ export function crearHabitacion(data) {
 
     } catch (error) {
       dispatch(postFailure());
+    } finally {
+      dispatch(postFinished());
     }
   }
 }
