57327eda0f62dbf1c468e08540fab312d2ae2d3a
Mmanna3 <matiasmannarino@gmail.com>
2020-06-26 00:58:29 -0300

    CleanErrors

--- Frontend/src/features/habitaciones/crear/slice.js
+++ Frontend/src/features/habitaciones/crear/slice.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 
 export const initialState = {
   loading: false,
-  hasSuccess: false,
-  hasErrors: false,
   requestData: '',
   responseData: '',
   validationErrors: undefined
@@ -20,17 +18,13 @@ const crearHabitacionSlice = createSlice({
     },
     postSuccess: state => {
       state.loading = false
-      state.hasSuccess = true
     },
     postFailure: (state, {payload}) => {
       state.loading = false
-      state.hasErrors = true
       state.validationErrors = payload?.errors
     },
-    postReset: (state) => {
+    reset: (state) => {
       state.loading = false
-      state.hasSuccess = false
-      state.hasErrors = false
       state.responseData = ''
       state.requestData = ''
       state.validationErrors = undefined
@@ -38,7 +32,7 @@ const crearHabitacionSlice = createSlice({
   },
 })
 
-export const { postInit, postSuccess, postFailure, postReset } = crearHabitacionSlice.actions
+export const { postInit, postSuccess, postFailure, reset } = crearHabitacionSlice.actions
 export const crearHabitacionSelector = state => state.crearHabitacion
 export default crearHabitacionSlice.reducer
 
@@ -61,6 +55,6 @@ export function crearHabitacion(data, onSuccess) {
 export function cleanErrors() {
   
   return async dispatch => {
-    dispatch(postReset());
+    dispatch(reset());
   }
 }
