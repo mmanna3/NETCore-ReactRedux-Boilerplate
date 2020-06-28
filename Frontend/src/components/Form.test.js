2a88b603c706a9778ee380921b1ce062d96a1f57
Mmanna3 <matiasmannarino@gmail.com>
2020-06-27 21:31:29 -0300

    primer test más o menos decente

---
+++ Frontend/src/components/Form.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16'
+import { mount, render, shallow, configure } from 'enzyme';
+import Form from './Form';
+import {Input} from './Input';
+import 'mutationobserver-shim';
+global.MutationObserver = window.MutationObserver;
+
+configure({ adapter: new Adapter() });
+
+it('un sólo tag input adentro, le pone el name', () => {
+  const jsx = (
+    <Form name="prueba">
+      <Input label="Nombre" name="nombre" />
+    </Form>
+)
+  
+  const wrapper = shallow(jsx);
+  
+  console.log(wrapper);
+  var name = wrapper.find(Input).prop('name');
+  console.log(name);
+  expect(name).toBe('nombre');
+  
+  
+
+  // console.log(wrapper.props())
+  // console.log(wrapper.props().children)
+  // console.log(wrapper.props().children.type)
+  // expect(wrapper.props().name).to.equal('prueba');
+  
+  //wrapper.setProps({ bar: 'foo' });
+  //expect(wrapper.props().bar).to.equal('foo');
+});
