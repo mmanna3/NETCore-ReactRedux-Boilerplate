c987ac712f154eccad821c4265959dae1d80ae49
Mmanna3 <matiasmannarino@gmail.com>
2020-07-10 02:29:04 -0300

    si el usuario no está autorizado, redirige al login

--- Frontend/src/components/axiosAuthenticated.js
+++ Frontend/src/components/axiosAuthenticated.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { createBrowserHistory } from 'history';
 
 let user = JSON.parse(localStorage.getItem('user'));
 
@@ -7,5 +8,15 @@ if (user && user.token)
   authToken = 'Bearer ' + user.token;
 
 axios.defaults.headers.common = {'Authorization': `${authToken}`}
+
+axios.interceptors.response.use(response => response, error => {
+  
+  if(error.response.status === 401){    
+    createBrowserHistory().push('/');
+    window.location.reload();
+  }
+  
+  return error;
+});
 
 export default axios;
