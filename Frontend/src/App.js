33f6b163968066c8d18b98f94978b7c9ff843d8f
Mmanna3 <matiasmannarino@gmail.com>
2020-07-11 21:55:24 -0300

    HuespedesPage

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,9 +7,12 @@ import {
 } from 'react-router-dom'
 
 import 'utils/FontAwesomeLibrary';
+
 import DashboardPage from './pages/DashboardPage'
 import PostsPage from './pages/PostsPage'
 import SinglePostPage from './pages/SinglePostPage'
+
+import HuespedesPage from 'features/huespedes/Page'
 import HabitacionesPage from 'features/habitaciones/Page'
 import LoginPage from 'features/login/Page'
 
@@ -19,6 +22,7 @@ const App = () => {
       {/* <Navbar /> */}
       <Switch>
         <Route exact path="/habitaciones" component={HabitacionesPage} />
+        <Route exact path="/huespedes" component={HuespedesPage} />
         <Route exact path="/" component={LoginPage} />
         <Route exact path="/dashboard" component={DashboardPage} />
         <Route exact path="/posts" component={PostsPage} />
---
+++ Frontend/src/features/huespedes/Page.js
@@ -0,0 +1,55 @@
+import React, { useState, useCallback } from 'react';
+import Table from 'components/Table'
+import { fetchHuespedes, huespedesSelector } from './slice'
+import { useDispatch, useSelector } from 'react-redux'
+
+const HuespedesPage = () => {
+  const dispatch = useDispatch();
+  const { datos, loading, hasErrors } = useSelector(huespedesSelector);
+
+  const fetchData = useCallback(() => {
+    dispatch(fetchHuespedes());
+  }, [dispatch]);
+
+  const columnas = [
+    {
+      Header: 'Nombre',
+      accessor: 'nombre',
+    }
+  ]
+  
+  const [IsModalVisible, setModalVisibility] = useState(false);
+
+  function closeModalAndRefreshTable() {
+    hideModal();
+    fetchData();
+  }
+
+  function hideModal(){
+    setModalVisibility(false);
+  }
+
+  function showModal(){
+    setModalVisibility(true);
+  }  
+
+  return (
+    <div className="container">
+        {/* <Crear isVisible={IsModalVisible} onHide={hideModal} onSuccessfulSubmit={closeModalAndRefreshTable}></Crear> */}
+        
+        <h1 className="title is-1">Huéspedes</h1>
+        <div className="buttons is-fullwidth is-pulled-right">
+          <button className="button is-primary" onClick={showModal}>Crear</button>
+        </div>        
+        <Table  fetchData={fetchData}
+                selector={huespedesSelector}
+                columnas={columnas}
+                datos={datos}
+                loading={loading}
+                hasErrors={hasErrors}
+        />
+    </div>
+  )
+}
+
+export default HuespedesPage;
