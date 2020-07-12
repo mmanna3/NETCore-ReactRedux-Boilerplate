b42ae2423d3a8fe24fcb6deabbfdc61170d6eedb
Mmanna3 <matiasmannarino@gmail.com>
2020-07-11 21:52:24 -0300

    Agrega Huespedes Slice

--- Frontend/src/slices/index.js
+++ Frontend/src/slices/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 
 import habitacionesReducer from 'features/habitaciones/slice'
+import huespedesReducer from 'features/huespedes/slice'
 import crearHabitacionReducer from 'features/habitaciones/crear/slice'
 import loginReducer from 'features/login/slice'
 
@@ -14,9 +15,11 @@ const rootReducer = combineReducers({
   login : loginReducer,
   crearHabitacion : crearHabitacionReducer,
   habitaciones: habitacionesReducer,
+  huespedes: huespedesReducer,
+
   posts: postsReducer,
   comments: commentsReducer,
   post: postReducer,
-})
+});
 
-export default rootReducer
+export default rootReducer;
