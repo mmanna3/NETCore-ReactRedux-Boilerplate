738f0e0d995cdd37a336dc0ad2f9242a200b3863
Mmanna3 <matiasmannarino@gmail.com>
2020-07-12 01:37:22 -0300

    Arregla warnings de react

--- Frontend/src/components/navbar/Navbar.js
+++ Frontend/src/components/navbar/Navbar.js
@@ -9,30 +9,29 @@ const Navbar = () => {
 
   return (
     <>
-
-      <nav class={`navbar is-primary ${styles.spaceAtBottom}`} role="navigation" aria-label="main navigation">
-        <div class="container">
-          <div class="navbar-brand">
-            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <nav className={`navbar is-primary ${styles.spaceAtBottom}`} role="navigation" aria-label="main navigation">
+        <div className="container">
+          <div className="navbar-brand">
+            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="/#">
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
             </a>
           </div>
 
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-              <Link class="navbar-item has-text-weight-medium" to="/habitaciones">Habitaciones</Link>
-              <Link class="navbar-item has-text-weight-medium" to="/huespedes">Huéspedes</Link>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+              <Link className="navbar-item has-text-weight-medium" to="/habitaciones">Habitaciones</Link>
+              <Link className="navbar-item has-text-weight-medium" to="/huespedes">Huéspedes</Link>
           </div>
 
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <p class="button is-primary">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <p className="button is-primary">
                   <span>¡Hola  </span><strong>{nombre}</strong>!
                 </p>
-                <a class="button is-primary is-inverted is-outlined">
+                <a className="button is-primary is-inverted is-outlined" href="/#">
                   Cerrar sesión
                 </a>
               </div>
@@ -42,7 +41,7 @@ const Navbar = () => {
       </div>
     </nav>
   </>
-)
+  )
 };
 
 export default Navbar;
