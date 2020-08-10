1d178114b95d85eea22b992c27948e8f6fe9b5ff
Mmanna3 <matiasmannarino@gmail.com>
2020-08-10 00:18:57 -0300

    thead: muestra habitaciones correctamente

--- Frontend/src/features/calendario/Table/Thead/Thead.test.js
+++ Frontend/src/features/calendario/Table/Thead/Thead.test.js
@@ -7,25 +7,36 @@ configure({ adapter: new Adapter() });
 
 describe('Thead', () => {
     
-    var camasPorHabitacion = [
-        {
-          nombre: 'Azul',
-          camas: ['Individual: A', 'Individual: B']
-        },
-        {
-          nombre: 'Roja',
-          camas: ['Matrimonial: 1', 'Mar. Arriba: 2', 'Mar. Abajo: 3']
-        },
-        {
-          nombre: 'Verde',
-          camas: ['Matrimonial: Matri', 'Individual: Indi']
-        }
-      ];
+  var camasPorHabitacion = [
+      {
+        nombre: 'Azul',
+        camas: ['Individual: A', 'Individual: B']
+      },
+      {
+        nombre: 'Roja',
+        camas: ['Matrimonial: 1', 'Mar. Arriba: 2', 'Mar. Abajo: 3']
+      },
+      {
+        nombre: 'Verde',
+        camas: ['Matrimonial: Matri', 'Individual: Indi']
+      }
+    ];
 
-    it('displays two rows', () => {
-        const wrapper = shallow(<Thead camasPorHabitacion={camasPorHabitacion} />);
-        const theadHtml = wrapper.find('thead');
-        const rows = theadHtml.find('tr');
-        expect(rows).toHaveLength(2);
-    })
+  it('tiene dos filas', () => {
+      const wrapper = shallow(<Thead camasPorHabitacion={camasPorHabitacion} />);
+      const theadHtml = wrapper.find('thead');
+      const rows = theadHtml.find('tr');
+      expect(rows).toHaveLength(2);
+  })
+
+  it('muestra habitaciones correctamente', () => {
+    const wrapper = shallow(<Thead camasPorHabitacion={camasPorHabitacion} />);
+    const theadHtml = wrapper.find('thead');
+    const habitacionesTr = theadHtml.find('tr').first();
+    const habitaciones = habitacionesTr.find('th');
+    expect(habitaciones).toHaveLength(4);
+    
+    const texts = habitaciones.map((node) => node.text());
+    expect(texts).toEqual(['', 'Habitación Azul', 'Habitación Roja', 'Habitación Verde']);
+  })
 })
