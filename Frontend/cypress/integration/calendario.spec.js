6ae6919396e084c47cb65f92a02485bc59ad2cfa
Mmanna3 <matiasmannarino@gmail.com>
2020-08-20 02:26:09 -0300

    Buen refactor

--- Frontend/cypress/integration/calendario.spec.js
+++ Frontend/cypress/integration/calendario.spec.js
@@ -1,99 +1,108 @@
-describe('Tests del calendario', () => {
+describe('Calendario', () => {
 
-    it('Ir a la opción de menú', () => {
-        cy.visit('http://localhost:3000/');
+    it('Selección básica', () => {
         
-        cy.get('[name="username"]')
-            .type('yo')
-
-        cy.get('[name="password"]')
-            .type('123')
-
-        cy.get('[type="submit"]')
-            .click()
-
-        cy.contains('a', 'Calendario')
-            .click()
-
-        cy.contains('h1', 'Calendario')
-            .should('be.visible')
+        acceder();
+        
+        irAlCalendario();        
         
         seleccionarDesdeHasta('00', '50');
-        QueEstenSeleccionadasDesdeHasta('00','50');        
+
+        queEstenSeleccionadasDesdeHasta('00','50');
     })
-    
-    const seleccionarDesdeHasta = (celdaInicialId, celdaFinalId) => {
-    
-        const celdasIdsEntre = (celdaInicialId, celdaFinalId) => {
-            var columna = celdaInicialId[1];
-            
-            var result = [];
-            var i = parseInt(celdaInicialId[0]) + 1;
-        
-            while (i < parseInt(celdaFinalId[0])) {            
-                result.push(`${i}${columna}`);
-                i++;            
-            }
-        
-            return result;
-        }
+})
 
+const acceder = () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('[name="username"]')
+        .type('yo');
+
+    cy.get('[name="password"]')
+        .type('123');
+
+    cy.get('[type="submit"]')
+        .click();
+}
+
+const irAlCalendario = () => {
+    cy.contains('a', 'Calendario')
+    .click()
+
+    cy.contains('h1', 'Calendario')
+        .should('be.visible')
+}
+
+
+const seleccionarDesdeHasta = (celdaInicialId, celdaFinalId) => {
+    
+    const celdasIdsEntre = (celdaInicialId, celdaFinalId) => { //Es igual a la otra, no la pude sacar
+        var columna = celdaInicialId[1];
         
-        var celdasIdsEntre = celdasIdsEntre(celdaInicialId, celdaFinalId);        
+        var result = [];
+        var i = parseInt(celdaInicialId[0]) + 1;
     
-        cy.get('#'+celdaInicialId)
-            .trigger('mouseover')
-            .trigger('mousedown', {which: 1})
-            .trigger('mousemove')        
+        while (i < parseInt(celdaFinalId[0])) {            
+            result.push(`${i}${columna}`);
+            i++;            
+        }
     
-        celdasIdsEntre.forEach(id => {
-            cy.get('#'+id)
-                .trigger('mouseover')            
-                .trigger('mousemove')
-        });
+        return result;
+    }
+
     
-        cy.get('#'+celdaFinalId)
+    var celdasIdsEntre = celdasIdsEntre(celdaInicialId, celdaFinalId);        
+
+    cy.get('#'+celdaInicialId)
+        .trigger('mouseover')
+        .trigger('mousedown', {which: 1})
+        .trigger('mousemove')        
+
+    celdasIdsEntre.forEach(id => {
+        cy.get('#'+id)
+            .trigger('mouseover')            
             .trigger('mousemove')
-            .trigger('mouseup', {force: true})
-    }
-    
-    
-    const QueEstenSeleccionadasDesdeHasta = (celdaInicialId, celdaFinalId) => {
-    
-        const celdasIdsEntre = (celdaInicialId, celdaFinalId) => {
-            var columna = celdaInicialId[1];
-            
-            var result = [];
-            var i = parseInt(celdaInicialId[0]) + 1;
+    });
+
+    cy.get('#'+celdaFinalId)
+        .trigger('mousemove')
+        .trigger('mouseup', {force: true})
+}
+
+
+const queEstenSeleccionadasDesdeHasta = (celdaInicialId, celdaFinalId) => {
+
+    const celdasIdsEntre = (celdaInicialId, celdaFinalId) => {  //Es igual a la otra, no la pude sacar
+        var columna = celdaInicialId[1];
         
-            while (i < parseInt(celdaFinalId[0])) {            
-                result.push(`${i}${columna}`);
-                i++;            
-            }
-        
-            return result;
+        var result = [];
+        var i = parseInt(celdaInicialId[0]) + 1;
+    
+        while (i < parseInt(celdaFinalId[0])) {            
+            result.push(`${i}${columna}`);
+            i++;            
         }
-        
-        var celdasIdsEntre = celdasIdsEntre(celdaInicialId, celdaFinalId);        
     
-        cy.get('#'+celdaInicialId)
-            .invoke('attr', 'class')
-            .should('contain', 'selected')
-            .should('contain', 'firstSelected');
-    
-        celdasIdsEntre.forEach(id => {
-            cy.get('#'+id)
-                .invoke('attr', 'class')
-                .should('contain', 'selected');
-        });
-    
-        cy.get('#'+celdaFinalId)
-            .invoke('attr', 'class')
-            .should('contain', 'selected')
-            .should('contain', 'lastSelected');
-    
+        return result;
     }
+    
+    var celdasIdsEntre = celdasIdsEntre(celdaInicialId, celdaFinalId);        
 
+    cy.get('#'+celdaInicialId)
+        .invoke('attr', 'class')
+        .should('contain', 'selected')
+        .should('contain', 'firstSelected');
 
-  })
+    celdasIdsEntre.forEach(id => {
+        cy.get('#'+id)
+            .invoke('attr', 'class')
+            .should('contain', 'selected');
+    });
+
+    cy.get('#'+celdaFinalId)
+        .invoke('attr', 'class')
+        .should('contain', 'selected')
+        .should('contain', 'lastSelected');
+
+}
 
