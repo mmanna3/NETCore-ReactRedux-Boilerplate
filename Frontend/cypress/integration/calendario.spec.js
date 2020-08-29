5dd93ccfa9a7d082bbdf4b9fe74ab08f009d727b
Mmanna3 <matiasmannarino@gmail.com>
2020-08-28 21:07:15 -0300

    tests

--- Frontend/cypress/integration/calendario.spec.js
+++ Frontend/cypress/integration/calendario.spec.js
@@ -1,12 +1,9 @@
 /*
 
 Puedo reservar un sólo día
-    una cama
-    2 camas
     3 camas
 Puedo resevar 2 días
     una cama
-    2 camas
     3 camas
 Puedo reservar más de 2 días
     una cama
@@ -15,14 +12,12 @@ Puedo reservar más de 2 días
 
 Al moverme hacia la celda de arriba, se deselecciona(n) la(s) celda(s) del día actual
     Durante reserva de una cama
-    Durante reserva de 2 camas
     Durante reserva de 3 camas
 
 No puedo seleccionar en la misma reserva camas de distintas habitaciones
 
 No puedo seleccionar camas ya reservadas ese día
     una cama
-    2 camas
     3 camas
 
     
@@ -50,6 +45,17 @@ describe('Poder reservar un día', () => {
             .should('contain', 'lastSelected')
             ;
     })
+
+    it('3 camas', () => {
+        
+        acceder();        
+        irAlCalendario();
+
+        seleccionarDesdeHasta({row:0,col:0},{row:2,col:0});
+
+        // queEstenSeleccionadasDesdeHasta({row:0,col:0},{row:2,col:0});
+        // queNoEsteSeleccionada(({row:1,col:0}));
+    })
 })
 
 const acceder = () => {
@@ -71,4 +77,73 @@ const irAlCalendario = () => {
 
     cy.contains('h1', 'Calendario')
         .should('be.visible')
+}
+
+const seleccionarDesdeHasta = (celdaInicial, celdaFinal) => {    
+    
+    var celdasIntermedias = celdasEntre(celdaInicial, celdaFinal);        
+
+    getCeldaCypress(celdaInicial)
+        .trigger('mouseover')
+        .trigger('mousedown', {which: 1})
+        .trigger('mousemove')        
+
+        celdasIntermedias.forEach(celda => {
+            getCeldaCypress(celda)
+                .trigger('mouseover')
+                .trigger('mousemove')
+    });
+
+    getCeldaCypress(celdaFinal)
+        .trigger('mousemove')
+        .trigger('mouseup', {force: true})
+}
+
+function getCeldaCypress(celda) {
+    return cy.get(`[row="${celda.row}"][column="${celda.col}"]`);
+}
+
+
+// const queNoEsteSeleccionada = (celda) => {
+//     cy.get('#'+celda)
+//         .invoke('attr', 'class')
+//         .should('not.contain', 'selected');
+// }
+
+// function queEstenSeleccionadasDesdeHasta(celdaInicialId, celdaFinalId) {
+    
+//     var celdasIdsEntre = celdasIdsEntre(celdaInicialId, celdaFinalId);        
+
+//     cy.get('#'+celdaInicialId)
+//         .invoke('attr', 'class')
+//         .should('contain', 'selected')
+//         .should('contain', 'firstSelected');
+
+//     celdasIdsEntre.forEach(id => {
+//         cy.get('#'+id)
+//             .invoke('attr', 'class')
+//             .should('contain', 'selected');
+//     });
+
+//     cy.get('#'+celdaFinalId)
+//         .invoke('attr', 'class')
+//         .should('contain', 'selected')
+//         .should('contain', 'lastSelected');
+
+// }
+
+function celdasEntre(celdaInicial, celdaFinal) {
+    
+    var result = [];
+    var i = parseInt(celdaInicial.column) + 1;
+
+    while (i < parseInt(celdaFinal.column)) {            
+        result.push({
+                        column: celdaInicial.column, 
+                        row: i
+                    });
+        i++;            
+    }
+
+    return result;
 }
