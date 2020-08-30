1cd17dc515ddde57fa3eff550da5c32aa71a1b83
Mmanna3 <matiasmannarino@gmail.com>
2020-08-30 16:26:06 -0300

    refactor

--- Frontend/src/features/calendario/reservasDelMes/slice.js
+++ Frontend/src/features/calendario/reservasDelMes/slice.js
@@ -14,24 +14,25 @@ const reservasDelMesSlice = createSlice({
     },
     seleccionarPrimeraCeldaAction: (state, {payload}) => {
       var copy = state.calendario;
-      copy[payload.row][payload.column] = crearCelda(selectedOptions.FIRST);
-      copy[payload.row+1][payload.column] = crearCelda(selectedOptions.NO, true);
+      copy[payload.row][payload.col] = crearCelda(selectedOptions.FIRST);
+      copy[payload.row+1][payload.col] = crearCelda(selectedOptions.NO, true);
       state.calendario = copy;
     },
     seleccionarUltimaCeldaAction: (state, {payload}) => {
       var copy = state.calendario;
-      copy[payload.row][payload.column] = crearCelda(selectedOptions.LAST);
+      copy[payload.row][payload.col] = crearCelda(selectedOptions.LAST);
+      
       state.calendario = copy;
     },
     seleccionarCeldaUnicaAction: (state, {payload}) => {
       var copy = state.calendario;
-      copy[payload.row][payload.column] = crearCelda(selectedOptions.UNIQUE);
+      copy[payload.row][payload.col] = crearCelda(selectedOptions.UNIQUE);
       state.calendario = copy;
     },
     seleccionarCeldaIntermediaAction: (state, {payload}) => {
       var copy = state.calendario;
-      copy[payload.row][payload.column] = crearCelda(selectedOptions.YES);
-      copy[payload.row+1][payload.column] = crearCelda(selectedOptions.NO, true);
+      copy[payload.row][payload.col] = crearCelda(selectedOptions.YES);
+      copy[payload.row+1][payload.col] = crearCelda(selectedOptions.NO, true);
       state.calendario = copy;
     }
   },
@@ -55,26 +56,26 @@ export function setInitialState(data) {
   }
 }
 
-export function seleccionarCeldaUnica(row, column) {
+export function seleccionarCeldaUnica(row, col) {
   return async dispatch => {
-    dispatch(seleccionarCeldaUnicaAction({row, column}));
+    dispatch(seleccionarCeldaUnicaAction({row, col}));
   }
 }
 
-export function seleccionarPrimeraCelda(row, column) {
+export function seleccionarPrimeraCelda(row, col) {
   return async dispatch => {
-    dispatch(seleccionarPrimeraCeldaAction({row, column}));
+    dispatch(seleccionarPrimeraCeldaAction({row, col}));
   }
 }
 
-export function seleccionarUltimaCelda(row, column) {
+export function seleccionarUltimaCelda(row, col) {
   return async dispatch => {
-    dispatch(seleccionarUltimaCeldaAction({row, column}));
+    dispatch(seleccionarUltimaCeldaAction({row, col}));
   }
 }
 
-export function seleccionarCeldaIntermedia(row, column) {
+export function seleccionarCeldaIntermedia(row, col) {
   return async dispatch => {
-    dispatch(seleccionarCeldaIntermediaAction({row, column}));
+    dispatch(seleccionarCeldaIntermediaAction({row, col}));
   }
 }
