6288b688240f6c12b89b388022350d626e85ed7e
Mmanna3 <matiasmannarino@gmail.com>
2020-09-23 17:27:56 -0300

    Refactor

--- Frontend/src/components/dateRangePicker/DateRangePicker.js
+++ Frontend/src/components/dateRangePicker/DateRangePicker.js
@@ -4,12 +4,16 @@ import Styles from './DateRangePicker.module.scss'
 import Drp from '@wojtekmaj/react-daterange-picker'
 
 const DateRangePicker = ({value, onChange}) => {  
-
-  //Porque como tiene muchos 9 en los milisegundos, al pasar a ISOString redondea a un día más
+  
   function ajustarFechaHasta(hasta) {
     var result = new Date(hasta);
-    result.setDate(result.getDate() -1);
-    return result;
+    //Porque como tiene muchos 9 en los milisegundos, al pasar a ISOString redondea a un día más
+    result.setDate(result.getDate()-1);    
+    return result.toISOString().slice(0,10);    
+  }
+
+  function ajustarFechaDesde(desde) {
+    return desde.toISOString().slice(0,10);
   }
 
   return (
@@ -25,8 +29,8 @@ const DateRangePicker = ({value, onChange}) => {
           minDate={new Date()}
         />
       </div>
-      <Input style={{display: 'none'}} name="desde" defaultValue={value[0].toISOString().slice(0,10)}/>
-      <Input style={{display: 'none'}} name="hasta" defaultValue={ajustarFechaHasta(value[1]).toISOString().slice(0,10)}/>
+      <Input style={{display: 'none'}} name="desde" defaultValue={ajustarFechaDesde(value[0])}/>
+      <Input style={{display: 'none'}} name="hasta" defaultValue={ajustarFechaHasta(value[1])}/>
     </div>
   )
 }
