915ff808d51829fe8d6d85295786700ccb36b3a8
Mmanna3 <matiasmannarino@gmail.com>
2020-09-26 18:58:52 -0300

    Refactor

--- Frontend/src/features/reservas/crear/SelectCama.js
+++ Frontend/src/features/reservas/crear/SelectCama.js
@@ -1,55 +1,8 @@
-import React from 'react';
-import { InputWithoutLabel } from "components/Input";    
+import React from 'react';   
 import Select from "components/Select";    
 import { Icon } from "components/Icon";    
 
 const SelectCama = ({habitaciones, cargando, camasDisponibles, onHabitacionChange}) => {
-
-  // const IdentificadorIndividualOMatrimonial = ({cama, setValue}) => {
-
-  //   return <div className="field">
-  //           <span className="control is-expanded">
-  //             <InputWithoutLabel 
-  //                         name={`camas${cama.tipo}[${cama.index}].nombre`} 
-  //                         placeholder="Identificador"
-  //                         onChange={e => setValue(cama.globalIndex, {nombre: e.target.value})}
-  //                         value={cama.value?.nombre}/>
-  //           </span>
-  //         </div>
-  // };
-
-  // const IdentificadorCamaCucheta = ({cama}) => {
-    
-  //   function setValueCucheta(){
-  //     var abajo = document.getElementsByName(`camasCuchetas[${cama.index}].abajo.nombre`)[0].value;
-  //     var arriba = document.getElementsByName(`camasCuchetas[${cama.index}].arriba.nombre`)[0].value;
-
-  //     setValue(cama.globalIndex, {abajo: {nombre: abajo}, arriba: {nombre: arriba}});
-  //   }
-    
-  //   return <>
-  //           <span className="field">
-  //               <InputWithoutLabel 
-  //                 name={`camasCuchetas[${cama.index}].abajo.nombre`} 
-  //                 placeholder="Id. Abajo"
-  //                 onChange={() => setValueCucheta()}
-  //                 value={cama.value?.abajo?.nombre}
-  //                 />
-  //             </span>
-  //             <span className="field">
-  //               <InputWithoutLabel 
-  //                 name={`camasCuchetas[${cama.index}].arriba.nombre`} 
-  //                 placeholder="Id. Arriba"
-  //                 onChange={() => setValueCucheta()}
-  //                 value={cama.value?.arriba?.nombre}
-  //               />
-  //             </span>
-  //         </>
-  // };  
-
-  // const onTipoCamaChanged = (e) => {
-  //   setTipoCama(cama.index, cama.tipo, e.target.value);
-  // }
 
   return (
     <div className="field field-body is-grouped">
@@ -67,7 +20,7 @@ const SelectCama = ({habitaciones, cargando, camasDisponibles, onHabitacionChang
               {cargando ?
               <option>Cargando...</option> :
               habitaciones.map((habitacion, index) => {
-                return <option key={habitacion.Id} value={index}>{habitacion.nombre} ({habitacion.cantidadDeLugaresLibres})</option>
+                return <option key={habitacion.id} value={index}>{habitacion.nombre} ({habitacion.cantidadDeLugaresLibres})</option>
               })
               }
               </Select>
