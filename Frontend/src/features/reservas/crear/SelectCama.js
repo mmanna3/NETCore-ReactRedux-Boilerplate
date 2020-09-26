7de0b950b230569f46d7cf69eebdac2abdec5910
Mmanna3 <matiasmannarino@gmail.com>
2020-09-26 18:56:25 -0300

    Estética

--- Frontend/src/features/reservas/crear/SelectCama.js
+++ Frontend/src/features/reservas/crear/SelectCama.js
@@ -52,51 +52,55 @@ const SelectCama = ({habitaciones, cargando, camasDisponibles, onHabitacionChang
   // }
 
   return (
-    <div>
-        <Select name="Habitacion" onChange={onHabitacionChange}>
-          {cargando ?
-            <option>Cargando habitación..</option> :
-            habitaciones.map((habitacion, index) => {
-              return <option key={habitacion.Id} value={index}>{habitacion.nombre} ({habitacion.cantidadDeLugaresLibres} lugares)</option>
-            })
-          }
-        </Select>
-        <Select name="CamasIds[0]">
-          {camasDisponibles.length === 0 ?
-            <option>No hay camas disponibles</option> :
-            camasDisponibles.map((cama) => {
-              return <option key={cama.id} value={cama.id}>{cama.tipo} - {cama.nombre}</option>
-            })
-          }
-        </Select>
+    <div className="field field-body is-grouped">
+
+      <div className="field field-body is-grouped">
+        <div className="field is-expanded has-addons" style={{minWidth:"200px"}}>
+          <span className="control">
+            <span className="button is-static">
+              Hab.
+            </span>
+          </span>
+          <span className="control is-expanded">
+            <span className="control is-expanded">
+              <Select name="Habitacion" style={{minWidth:"180px"}} onChange={onHabitacionChange}>
+              {cargando ?
+              <option>Cargando...</option> :
+              habitaciones.map((habitacion, index) => {
+                return <option key={habitacion.Id} value={index}>{habitacion.nombre} ({habitacion.cantidadDeLugaresLibres})</option>
+              })
+              }
+              </Select>
+            </span>
+          </span>
+        </div>
+
+        <div className="field is-expanded has-addons" style={{minWidth:"280px"}}>
+          <span className="control">
+            <span className="button is-static">
+              Cama
+            </span>
+          </span>
+          <span className="control is-expanded">
+            <span className="control is-expanded">
+            <Select name="CamasIds[0]" style={{minWidth:"220px"}}>
+              {camasDisponibles.length === 0 ?
+                <option>No tiene</option> :
+                camasDisponibles.map((cama) => {
+                  return <option key={cama.id} value={cama.id}>{cama.tipo} - {cama.nombre}</option>
+                })
+              }
+            </Select>
+            </span>
+          </span>
+        </div>
+      </div>
+
+      <button className="button has-text-grey has-background-light" type="button" onClick={() => console.log('aas')}>
+        <Icon faCode="trash-alt" />
+      </button>
+
     </div>
-    // <div className="field field-body is-grouped">
-
-    //     <div className="field is-expanded has-addons" style={{minWidth:"200px"}}>
-    //       <span className="control">
-    //         <span className="button is-static">
-    //           Cama
-    //         </span>
-    //       </span>
-    //       <span className="control is-expanded">
-    //         <Select value={cama.tipo || ''} ccsClass="is-fullwidth" onChange={onTipoCamaChanged}>
-    //           <option value="Individuales">Individual</option>
-    //           <option value="Matrimoniales">Matrimonial</option>
-    //           <option value="Cuchetas">Cucheta</option>
-    //         </Select>
-    //       </span>
-    //     </div>
-        
-    //     {cama.tipo !== 'Cuchetas' ?
-    //       <IdentificadorIndividualOMatrimonial cama={cama} setValue={setValue} /> :
-    //       <IdentificadorCamaCucheta cama={cama} setValue={setValue}/>
-    //     }
-        
-    //     <button className="button has-text-grey has-background-light" type="button" onClick={removeCama(cama.globalIndex)}>
-    //         <Icon faCode="trash-alt" />
-    //     </button>
-
-    // </div>
   )
 }
 
