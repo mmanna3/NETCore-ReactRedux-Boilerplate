b7c80e7c3448ccaa57bc699ea53d45cc65ff2c4f
Mmanna3 <matiasmannarino@gmail.com>
2020-10-07 19:28:48 -0300

    Camas en cabecera

--- Frontend/src/features/reservas/Tabla/Tabla.js
+++ Frontend/src/features/reservas/Tabla/Tabla.js
@@ -3,24 +3,31 @@ import Estilos from './Tabla.module.scss'
 
 const TablaReservas = ({datos, mes}) => {
 
-  
   let filas = [];
   for (let i = 0; i < datos.diasDelMes; i++) {            
     filas.push(<tr key={i}><td>{i+1}/{mes}</td></tr>);
-  }    
+  }
 
-  return (
-      <table className={`table is-hoverable is-bordered is-fullwidth ${Estilos.tabla}`}>
-        <thead>
-          <tr>
-            <th>Día</th>
-          </tr>          
-        </thead>
-        <tbody>
-          {filas}
-        </tbody>
-      </table>
-  )
+  //Esto está feo, arreglalo.
+  if (datos.camas)
+    
+    return (
+        <table className={`table is-hoverable is-bordered is-fullwidth ${Estilos.tabla}`}>
+          <thead>
+            <tr>
+              <th>Día</th>
+              {datos.camas.map((cama) => 
+                <th key={cama.id}>{cama.nombre} - {cama.tipo}</th>
+              )}
+            </tr>          
+          </thead>
+          <tbody>
+            {filas}
+          </tbody>
+        </table>
+    )
+  else
+      return null;
 }
 
 export default TablaReservas;
