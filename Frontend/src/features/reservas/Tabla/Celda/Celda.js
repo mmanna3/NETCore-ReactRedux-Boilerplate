68e58af3ffe2b186754f88ecb8fbc6b0b5c68fb5
Mmanna3 <matiasmannarino@gmail.com>
2020-10-09 19:42:27 -0300

    Relaciona celda con tabla de redux

--- Frontend/src/features/reservas/Tabla/Celda/Celda.js
+++ Frontend/src/features/reservas/Tabla/Celda/Celda.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { tablaDeReservasSelector } from 'features/reservas/Tabla/slice'
+import { useSelector } from 'react-redux'
 
 const Celda = ({dia, camaId}) => {
 
+    const {tabla} = useSelector(tablaDeReservasSelector);
+
     return (
-        <td dia={dia} camaid={camaId}></td>
+        <td dia={dia} camaid={camaId}>{tabla[dia-1][`${camaId}`]}</td>
     )
 }
 
