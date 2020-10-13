6e8c42892946f425ed5548e6bc036c5bd2e79982
Mmanna3 <matiasmannarino@gmail.com>
2020-10-12 21:23:35 -0300

    Agrega selector de mes en reservas

--- Frontend/src/features/reservas/Page.js
+++ Frontend/src/features/reservas/Page.js
@@ -4,21 +4,22 @@ import { fetchHabitaciones, habitacionesSelector } from 'features/habitaciones/s
 import { useDispatch, useSelector } from 'react-redux'
 import Crear from './crear/Modal'
 import {Button} from 'components/Buttons'
+import Select from 'components/Select'
 import Tabla from './Tabla/Tabla'
 
 const ReservasPage = () => {
   const dispatch = useDispatch();
   const { datos, estaCargando, tieneErrores } = useSelector(reservasSelector);
   const habitaciones = useSelector(habitacionesSelector);
+  const [IsModalVisible, setModalVisibility] = useState(false);
+  const [mes, actualizarMes] = useState(new Date().getMonth()+1);
 
   const fetchData = useCallback(() => {
-    dispatch(fetchReservas(2020, 10));
+    dispatch(fetchReservas(2020, mes));
     dispatch(fetchHabitaciones());
-  }, [dispatch]);
+  }, [dispatch, mes]);
 
-  useEffect(() => fetchData(), [fetchData]);
-  
-  const [IsModalVisible, setModalVisibility] = useState(false);
+  useEffect(() => fetchData(), [fetchData, mes]);  
 
   function closeModalAndRefreshTable() {
     hideModal();
@@ -31,20 +32,34 @@ const ReservasPage = () => {
 
   function showModal(){
     setModalVisibility(true);
-  }  
+  }
 
   return (
     <div className="container">
         <Crear isVisible={IsModalVisible} onHide={hideModal} onSuccessfulSubmit={closeModalAndRefreshTable}></Crear>
-        
         <h1 className="title is-1">Reservas</h1>
+        <Select defaultValue={mes} onChange={(e) => actualizarMes(e.target.value)}>
+          <option value="1">Enero</option>
+          <option value="2">Febrero</option>
+          <option value="3">Marzo</option>
+          <option value="4">Abril</option>
+          <option value="5">Mayo</option>
+          <option value="6">Junio</option>
+          <option value="7">Julio</option>
+          <option value="8">Agosto</option>
+          <option value="9">Septiembre</option>
+          <option value="10">Octubre</option>
+          <option value="11">Noviembre</option>
+          <option value="12">Diciembre</option>
+        </Select>
         <div className="buttons is-fullwidth is-pulled-right">
-          <Button onClick={showModal} text="Cargar nueva" />
+          <Button onClick={showModal} text="Cargar nueva"/>          
         </div>
         
       {/*Esto no está tan mal, pero igual está mal. */}
-      {tieneErrores? "Hubo un error." : ((estaCargando || datos.length === 0 || habitaciones.datos === 0) ? "Cargando..." : <Tabla datos={datos} habitaciones={habitaciones.datos} mes="10"/>)}
-        
+      <div>
+        {tieneErrores? "Hubo un error." : ((estaCargando || datos.length === 0 || habitaciones.datos === 0) ? "Cargando..." : <Tabla datos={datos} habitaciones={habitaciones.datos} mes={mes}/>)}
+      </div>
     </div>
   )
 }
