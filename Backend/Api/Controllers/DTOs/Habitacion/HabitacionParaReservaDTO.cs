904981027e05d73e20691e7284b2070eeefa4d6e
Mmanna3 <matiasmannarino@gmail.com>
2020-10-25 22:29:28 -0300

    be devuelve si hab es privada o no en listado con lugares libresw

--- Backend/Api/Controllers/DTOs/Habitacion/HabitacionParaReservaDTO.cs
+++ Backend/Api/Controllers/DTOs/Habitacion/HabitacionParaReservaDTO.cs
@@ -5,12 +5,9 @@ namespace Api.Controllers.DTOs.Habitacion
     public class HabitacionParaReservaDTO
     {
         public int Id { get; set; }
-
-        [YKNRequired, YKNStringLength(Maximo = 30)]
         public string Nombre { get; set; }
-
+        public bool EsPrivada { get; set; }
         public List<CamaDTO> Camas { get; set; }
-
         public int CantidadDeLugaresLibres { get; set; }
     }
 }
--- Backend/Api/Core/Models/Habitacion.cs
+++ Backend/Api/Core/Models/Habitacion.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using Api.Controllers.DTOs;
 
 namespace Api.Core.Models
 {
