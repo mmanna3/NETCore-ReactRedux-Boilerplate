2c52a3c7ad3eede98a1e6d923dd1f10126370672
Mmanna3 <matiasmannarino@gmail.com>
2020-11-29 15:01:34 -0300

    Ocultar checkouts al darle click a la cruz

--- Frontend/src/features/reservas/CheckoutsDeHoy/Componente.js
+++ Frontend/src/features/reservas/CheckoutsDeHoy/Componente.js
@@ -1,12 +1,14 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import { fetchCheckoutsDeHoy, checkoutsDeHoySelector } from './slice'
 import { useDispatch, useSelector } from 'react-redux'
 import ESTADOS from 'redux/estadosFetch'
+import Estilos from './Componente.module.scss'
 
 const CheckoutsDeHoy = () => {
 
   const dispatch = useDispatch();
   const { datos, estado } = useSelector(checkoutsDeHoySelector);
+  const [visible, mostrar] = useState(true)
 
   const fetchData = useCallback(() => {
     dispatch(fetchCheckoutsDeHoy())
@@ -16,25 +18,28 @@ const CheckoutsDeHoy = () => {
 
 
 
-  return (
-    <div className="notification is-primary is-light">
-      <button className="delete"></button>
+  if (visible)
+    return (
+      <div className="notification is-primary is-light">
+        <button className={`delete ${Estilos.botonSalir}`} onClick={() => mostrar(false)}></button>
 
-      {estado === ESTADOS.huboError ? "Hubo un error." : 
-          estado === ESTADOS.cargando ? "Cargando..." :
-            estado === ESTADOS.exitoso ? 
-              <>
-                <strong>Checkouts de hoy</strong>
-                <ul>
-                  {datos.map(e => 
-                    <li>{e.aNombreDe} (Habiación XXXXX)</li>
-                  )}                  
-                </ul>
-              </>
-              :
-              <></>}
-    </div>
+        {estado === ESTADOS.huboError ? "Hubo un error." : 
+            estado === ESTADOS.cargando ? "Cargando..." :
+              estado === ESTADOS.exitoso ? 
+                <>
+                  <strong>Checkouts de hoy</strong>
+                  <ul>
+                    {datos.map(e => 
+                      <li>{e.aNombreDe}</li>
+                    )}                  
+                  </ul>
+                </>
+                :
+                <></>}
+      </div>
   )
+  else
+    return <></>
 }
 
 export default CheckoutsDeHoy;
