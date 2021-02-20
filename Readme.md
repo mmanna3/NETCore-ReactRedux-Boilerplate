86c38461f2afe3fba89d94dfdaca661686cbdbe8
mmmannna <mmmannna3@gmail.com>
2021-02-20 15:07:18 -0300

    Readme con creación de acceso directo y BuenDia ahora también abre el vs code

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Crear acceso directo a BuenDia.Ps1
+
+1. Click derecho y crear acceso directo
+2. En location poner:
+```
+powershell.exe -ExecutionPolicy Bypass -File C:\Users\matia\source\repos\BlueServant\Scripts\BuenDia.ps1
+```
--- Scripts/BuenDia.ps1
+++ Scripts/BuenDia.ps1
@@ -1,3 +1,4 @@
-#Levanta servidores backend, frontend y una consola para git o lo que sea que quieras
+#Abre VS Code. Levanta servidores backend, frontend y una consola para git o lo que sea que quieras
 $carpetaActual = $PSScriptRoot
+code "${carpetaActual}\.."
 Start-Process wt "new-tab ""powershell"" powershell '${carpetaActual}\IniciarServidorBackend.ps1' `; new-tab ""powershell"" powershell '${carpetaActual}\IniciarServidorFrontend.ps1' `; split-pane ""powershell"" -noexit cd '${carpetaActual}\..'"
