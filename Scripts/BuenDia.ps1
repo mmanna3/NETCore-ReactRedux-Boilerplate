7dbdce528d112415fcd5fa90cb26b32681e0f0f8
mmmannna <mmmannna3@gmail.com>
2021-02-20 14:53:42 -0300

    Renombro todo a castellano porque ya demasiado colonizados estamos

---
+++ Scripts/BuenDia.ps1
@@ -0,0 +1,3 @@
+#Levanta servidores backend, frontend y una consola para git o lo que sea que quieras
+$carpetaActual = $PSScriptRoot
+Start-Process wt "new-tab ""powershell"" powershell '${carpetaActual}\IniciarServidorBackend.ps1' `; new-tab ""powershell"" powershell '${carpetaActual}\IniciarServidorFrontend.ps1' `; split-pane ""powershell"" -noexit cd '${carpetaActual}\..'"
--- Scripts/Develop-FE.ps1
+++
@@ -1,2 +0,0 @@
-$carpetaActual = $PSScriptRoot
-Start-Process wt "new-tab ""powershell"" powershell '${carpetaActual}\StartBackendServer.ps1' `; new-tab ""powershell"" powershell '${carpetaActual}\StartFrontendServer.ps1' `; split-pane ""powershell"" -noexit cd '${carpetaActual}\..'"
