721adc3ff5923ee0b1928e6a359e362468dc16db
mmmannna <mmmannna3@gmail.com>
2021-02-21 19:27:55 -0300

    BuenDia.ps1 abre carpeta Frontend en VS Code

--- Scripts/BuenDia.ps1
+++ Scripts/BuenDia.ps1
@@ -1,4 +1,4 @@
 #Abre VS Code. Levanta servidores backend, frontend y una consola para git o lo que sea que quieras
 $carpetaActual = $PSScriptRoot
-code "${carpetaActual}\.."
+code "${carpetaActual}\..\Frontend"
 Start-Process wt "new-tab ""powershell"" powershell '${carpetaActual}\IniciarServidorBackend.ps1' `; new-tab ""powershell"" powershell '${carpetaActual}\IniciarServidorFrontend.ps1' `; split-pane ""powershell"" -noexit cd '${carpetaActual}\..'"
