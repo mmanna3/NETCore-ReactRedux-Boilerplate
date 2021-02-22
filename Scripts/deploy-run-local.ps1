9459dd33942bb59598cd7b613ded8d7ef3325675
mmmannna <mmmannna3@gmail.com>
2021-02-22 13:52:25 -0300

    Reemplaza npm por yarn

--- Scripts/deploy-run-local.ps1
+++ Scripts/deploy-run-local.ps1
@@ -1,11 +1,12 @@
 # Yo creo que éste habría que revisarlo. Hoy no tuve ganas.
-cd ..
+
+Set-Location ..
 dotnet restore ./Backend/Api.sln --verbosity m
-cd Frontend
-npm install
-npm run build
-cd ..
+Set-Location Frontend
+yarn install
+yarn run build
+Set-Location ..
 Remove-Item -Recurse -Force "./Backend/Api/ClientApp/build"
 Copy-Item -Path "./Frontend/build" -Destination "./Backend/Api/ClientApp/build" -Recurse
-cd Backend/Api
+Set-Location Backend/Api
 dotnet run
