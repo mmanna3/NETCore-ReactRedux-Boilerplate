3574b0a493b5703ee491610724095f854e016b33
mmmannna <mmmannna3@gmail.com>
2021-03-13 23:21:16 -0300

    Arregla varias cosas de typescript para los slice

--- Frontend/src/features/habitaciones/slice.ts
+++ Frontend/src/features/habitaciones/slice.ts
@@ -1,13 +1,15 @@
 import { createSlice, fetchFunc } from 'redux/defaultFetchSlice';
 import { HabitacionDTO } from 'interfaces/habitacion';
+import { AxiosResponse } from 'axios';
+import { Dispatch } from '@reduxjs/toolkit';
 
 const nombre = 'habitaciones';
 const slice = createSlice(nombre);
 
-export const habitacionesSelector = (state: any) => state[nombre];
+export const habitacionesSelector = (state: any): any => state[nombre];
 export default slice.reducer;
 
-export function fetchHabitaciones() {
+export function fetchHabitaciones(): (dispatch: Dispatch) => Promise<AxiosResponse<HabitacionDTO>> {
   const endpoint = '/habitaciones';
   return fetchFunc<HabitacionDTO>(endpoint, slice.actions);
 }
--- Frontend/src/redux/defaultFetchSlice.ts
+++ Frontend/src/redux/defaultFetchSlice.ts
@@ -1,43 +1,48 @@
-import { createSlice as createSliceRTK } from '@reduxjs/toolkit'
-import axios from 'axios'
-import ESTADOS from './estadosFetch'
+import { createSlice as createSliceRTK, Slice, Dispatch } from '@reduxjs/toolkit';
+import axios, { AxiosResponse } from 'axios';
+import ESTADOS from './estadosFetch';
 
 const initialState = {
   estado: ESTADOS.inactivo,
   datos: [],
-}
+};
 
-export const createSlice = (nombre: string) => 
+export const createSlice = (nombre: string): Slice =>
   createSliceRTK({
     name: nombre,
     initialState,
     reducers: {
-      fetchInit: state => {
-        state.estado = ESTADOS.cargando
+      fetchInit: (state): void => {
+        state.estado = ESTADOS.cargando;
       },
-      fetchSuccess: (state, { payload }) => {
-        state.datos = payload
-        state.estado = ESTADOS.exitoso
+      fetchSuccess: (state, { payload }): void => {
+        state.datos = payload;
+        state.estado = ESTADOS.exitoso;
       },
-      fetchFailure: state => {
-        state.estado = ESTADOS.huboError
+      fetchFailure: (state): void => {
+        state.estado = ESTADOS.huboError;
       },
     },
   });
 
-export function fetchFunc<T>(endpoint: string, actions: any, onSuccessCallback?: () => void) {
-  const { fetchInit, fetchSuccess, fetchFailure } = actions
-  
-  return async (dispatch: any) => {
+export function fetchFunc<T>(
+  endpoint: string,
+  actions: any,
+  onSuccessCallback?: () => void
+): (dispatch: Dispatch) => Promise<AxiosResponse<T>> {
+  const { fetchInit, fetchSuccess, fetchFailure } = actions;
+
+  return async (dispatch: Dispatch): Promise<any> => {
     dispatch(fetchInit());
 
-    axios.get<T[]>('/api'+endpoint)
-    .then((res) => {      
-      dispatch(fetchSuccess(res.data));
-      typeof onSuccessCallback === 'function' && onSuccessCallback();
-    })
-    .catch((error) => {
+    axios
+      .get<T[]>('/api' + endpoint)
+      .then((res): void => {
+        dispatch(fetchSuccess(res.data));
+        typeof onSuccessCallback === 'function' && onSuccessCallback();
+      })
+      .catch((error): void => {
         dispatch(fetchFailure(error.response.data));
-    })
-  }
+      });
+  };
 }
