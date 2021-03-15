39b3df51dd1e57996d11cd791ebc7889207e5c37
mmmannna <mmmannna3@gmail.com>
2021-03-14 23:50:11 -0300

    Refactor por prettier

--- Frontend/src/features/reservas/CheckoutsDeHoy/Componente.js
+++ Frontend/src/features/reservas/CheckoutsDeHoy/Componente.js
@@ -1,44 +1,43 @@
-import React, {useCallback, useEffect, useState} from 'react'
-import { fetchCheckoutsDeHoy, checkoutsDeHoySelector } from './slice'
-import { useDispatch, useSelector } from 'react-redux'
-import ESTADOS from 'redux/estadosFetch'
-import { BotonSalir } from 'components/botones/botones'
+import React, { useCallback, useEffect, useState } from 'react';
+import { fetchCheckoutsDeHoy, checkoutsDeHoySelector } from './slice';
+import { useDispatch, useSelector } from 'react-redux';
+import ESTADOS from 'redux/estadosFetch';
+import { BotonSalir } from 'components/botones/botones';
 
 const CheckoutsDeHoy = () => {
-
   const dispatch = useDispatch();
   const { datos, estado } = useSelector(checkoutsDeHoySelector);
-  const [visible, mostrar] = useState(true)
+  const [visible, mostrar] = useState(true);
 
   const fetchData = useCallback(() => {
-    dispatch(fetchCheckoutsDeHoy())
+    dispatch(fetchCheckoutsDeHoy());
   }, [dispatch]);
 
   useEffect(() => fetchData(), [fetchData]);
 
-
-
   if (visible)
     return (
-      <div className="notification is-primary is-light">        
+      <div className="notification is-primary is-light">
         <BotonSalir onClick={() => mostrar(false)} />
-        {estado === ESTADOS.huboError ? "Hubo un error." : 
-            estado === ESTADOS.cargando ? "Cargando..." :
-              estado === ESTADOS.exitoso ? 
-                <>
-                  <strong>Checkouts de hoy</strong>
-                  <ul>
-                    {datos.map(e => 
-                      <li>{e.aNombreDe}</li>
-                    )}                  
-                  </ul>
-                </>
-                :
-                <></>}
+        {estado === ESTADOS.huboError ? (
+          'Hubo un error.'
+        ) : estado === ESTADOS.cargando ? (
+          'Cargando...'
+        ) : estado === ESTADOS.exitoso ? (
+          <>
+            <strong>Checkouts de hoy</strong>
+            <ul>
+              {datos.map((e, i) => (
+                <li key={i}>{e.aNombreDe}</li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
-  )
-  else
-    return <></>
-}
+    );
+  else return <></>;
+};
 
 export default CheckoutsDeHoy;
